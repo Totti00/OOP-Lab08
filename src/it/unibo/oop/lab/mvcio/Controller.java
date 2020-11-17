5685c8a14c39a6e0f4db2418a4e64f89babb3f94
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-11-17 22:23:11 +0100

    Finito es 3

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,10 +1,18 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 /**
  * 
  */
 public class Controller {
 
+    private static final String PATH = System.getProperty("user.home")
+                                + System.getProperty("file.separator");
+    private static final String DESTINATION = "output.txt";
+    private File file = new File(PATH + DESTINATION);
     /*
      * This class must implement a simple controller responsible of I/O access. It
      * considers a single file at a time, and it is able to serialize objects in it.
@@ -12,15 +20,36 @@ public class Controller {
      * Implement this class with:
      * 
      * 1) A method for setting a File as current file
-     * 
+     */
+    public final void setFile(final File file) {
+        this.file = file;
+    }
+    /* 
      * 2) A method for getting the current File
-     * 
-     * 3) A method for getting the path (in form of String) of the current File
-     * 
-     * 4) A method that gets a String as input and saves its content on the current
+     */
+    public final File getFile() {
+        return this.file;
+    }
+     /* 
+      * 3) A method for getting the path (in form of String) of the current File
+      */
+     public final String getPath() {
+         return this.file.getPath();
+     }
+
+    /* 4) A method that gets a String as input and saves its content on the current
      * file. This method may throw an IOException.
-     * 
-     * 5) By default, the current file is "output.txt" inside the user home folder.
+     */
+     public final void saveContent(final String line) {
+         try (PrintStream ps = new PrintStream(this.file)) {
+             ps.print(line);
+         } catch (FileNotFoundException e1) {
+             System.err.println(e1);
+             e1.printStackTrace();
+         }
+     }
+
+     /* 5) By default, the current file is "output.txt" inside the user home folder.
      * A String representing the local user home folder can be accessed using
      * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
      * Windows) can be obtained as String through the method
--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,9 +1,15 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -11,8 +17,8 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
-
+    private final JFrame frame = new JFrame("SimpleGUI");
+    private final Controller controller = new Controller();
     /*
      * Once the Controller is done, implement this class in such a way that:
      * 
@@ -37,6 +43,19 @@ public final class SimpleGUI {
      * builds a new {@link SimpleGUI}.
      */
     public SimpleGUI() {
+        final JPanel mioPanel = new JPanel();
+        mioPanel.setLayout(new BorderLayout());
+        final JButton save = new JButton("Salva");
+        final JTextArea text = new JTextArea();
+        mioPanel.add(text);
+        mioPanel.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(mioPanel);
+
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                SimpleGUI.this.controller.saveContent(text.getText());
+            }
+        });
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -57,6 +76,15 @@ public final class SimpleGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    }
+
+    private void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args) {
+        new SimpleGUI().display();
     }
 
 }
